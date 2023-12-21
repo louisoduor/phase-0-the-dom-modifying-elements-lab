6fb1b1cc8be5814994ea36102f5866226110290a
louisoduor <louisodhiambo20@gmail.com>
2023-12-21 08:48:01 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 // Write your code here!
-const element = document.createElement("div");
-document.body.append(element);
 
-const ul = document.createElement("ul");
+document.querySelector('main#main').remove();
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
+const newHeader = document.createElement('h1');
 
-  li.textContent = "Hi there!";
+newHeader.id = 'victory';
 
-console.log(li.textContent);
+newHeader.textContent = "Kevin is the champion"; 
 
-}
-
-element.append(ul);
-
-
-const main = document.getElementById("main");
-main.innerHTML =
-  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-
-  element.textContent = "You've changed what's on the screen!";
-
-  element.style.fontSize = "24px";
-  element.style.marginLeft = "30px";
-  element.style.lineHeight = 2;
-
-  element.className = "pet-listing dog";
-
-  element.classList.remove("dog");
-  element.classList.add("cat", "sale");
-
-  document.getElementsByTagName("ul")[0];
-  const secondChild = ul.querySelector("li:nth-child(2)");
-  ul.removeChild(secondChild);
-  
